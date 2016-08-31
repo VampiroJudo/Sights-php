2aae56062d87cfe02cbb0c96a711687ded676b93
John <johnconley507@hotmail.com>
2016-08-31 16:08:44 -0400

    string functions practice

--- sandbox/strings_functions.php
+++ sandbox/strings_functions.php
@@ -7,8 +7,25 @@
 	<body>
 		<?php
 
-		
+		$first = "The quick brown fox";
+		$second = " jumped over the lazy dog.";
+		$third = $first;
+		$third .= $second;
+		echo $third;
 
 		?>
+
+		<br />
+		Lowercase: <?php echo strtolower($third); ?><br>
+		Uppercase: <?php echo strtoupper($third); ?><br />
+		Uppercase first: <?php echo ucfirst($third); ?><br ?>
+		Uppercase: words: <?php echo ucwords($third); ?><br />
+		<br />
+		Length: <?php echo strlen($third); ?><br />
+		Trim: <?php echo "A" . trim(" B C D ") . "E"; ?><br />
+		Find: <?php echo strstr($third, "brown"); ?><br />
+		Replace by string: <?php echo str_replace("quick", "super-fast", $third); ?><br /> 
+
+
 	</body>
 </html>
