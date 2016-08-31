79dfac363be5009fb79e44ecac6e6f15ef495d60
John <johnconley507@hotmail.com>
2016-08-30 22:58:56 -0400

    First php tags

---
+++ helloworld.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Hello World</title>
+	</head>
+	<body>
+		<?php echo "Hello World!"; ?><br />
+		<?php echo "Hello" . "World!"; ?><br />
+		<?php echo 2 + 3; ?>
+	</body>
+</html>
