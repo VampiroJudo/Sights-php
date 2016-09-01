49f3061b4be9f60743d561f67ec7e647fe7f18ff
John <johnconley507@hotmail.com>
2016-09-01 14:32:14 -0400

    uploading integers practice

--- sandbox/integers.php
+++ sandbox/integers.php
@@ -4,6 +4,24 @@
 		<title>Integers</title>
 	</head>
 	<body>
+	<?php 
+		$var1 = 3;
+		$var2 = 4;
+	?>
+
+	Basic Math: <?php echo ((1 + 2 +$var1) * $var2) / 2 - 5; ?><br />
+
+	Absolute Value: <?php echo abs(0 - 300); ?><br />
+	Exponential: <?php echo pow(2,8); ?><br />
+	Square Root: <?php echo sqrt(100); ?><br />
+	Modulo: <?php echo fmod(20,7); ?><br />
+	Random: <?php echo rand(); ?><br />
+	Random (min,mix): <?php echo rand(1,10); ?><br />
+	<br />
+	+= : <?php $var2 +=4; echo $var2 ?><br />
+	-= : <?php $var2 -=4; echo $var2 ?><br />
+	*= : <?php $var2 *=4; echo $var2 ?><br />
+	/= : <?php $var2 /=4; echo $var2 ?><br />
 
 	</body>
 </html>
