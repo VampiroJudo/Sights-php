85f002f1dab38501785d2bccd9cbb5c268a9352c
John <johnconley507@hotmail.com>
2016-09-01 14:50:07 -0400

    uploading floating numbers file

---
+++ sandbox/floats.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Basic HTML</title>
+	</head>
+	<body>
+		<?php echo $float = 3.14; ?><br />
+		<?php echo $float + 7; ?><br />
+		<?php echo 4/3; ?><br />
+		<br />
+		Round: <?php echo round($float, 1); ?><br ?>
+		Ceiling: <?php echo ceil($float); ?><br />
+		Floor: <?php echo floor($float); ?><br />
+		<br />
+
+	</body>
+</html>
