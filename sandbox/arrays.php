6fb408a008d5acd4be3714e9ebb592e3f7d354bc
John <johnconley507@hotmail.com>
2016-09-01 15:18:26 -0400

    array practice

---
+++ sandbox/arrays.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Arrays</title>
+	</head>
+	<body>
+		<?php
+			$numbers = array(4,8,15,16,23,42);
+			echo $numbers[0];
+		?>
+		<br ?>
+
+		<?php $mixed = array(6, "fox", "dog",array("x","y", "z")); ?>
+		<?php echo $mixed[2]; ?><br />
+		<?php echo $mixed[3]; ?><br />
+
+
+	</body>
+</html>
