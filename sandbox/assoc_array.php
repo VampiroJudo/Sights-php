21c4cc8e86b71be1d295b926605f2fce1ca7ce0e
John <johnconley507@hotmail.com>
2016-09-02 16:58:14 -0400

    uploading associative array practice PHP

--- sandbox/assoc_array.php
+++ sandbox/assoc_array.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Basic HTML</title>
+		<title>Associative Array</title>
 	</head>
 	<body>
-	
+		<?php $assoc = array("first_name" => "John", "last_name" => "Conley");
+		?>
+		<?php echo $assoc["first_name"]; ?><br />
+		<?php echo $assoc["first_name"] . " " . $assoc["last_name"];?><br />
+		<?php $assoc["first_name"] = "Larry"; ?>
+		<?php echo $assoc["first_name"] . " " . $assoc["last_name"]; ?><br />
+
+		<?php $numbers = array(4,8,15,16,23,42); ?>
+		<?php $numbers = array(0 => 4, 1 => 8,2 => 15, 3 => 16, 4 => 23, 5 => 42); ?>
+		<?php echo $numbers[0]; ?>	
 
 	</body>
 </html>
