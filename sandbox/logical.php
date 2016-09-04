57df0df2219a0fdacafe66922208380fe1ea464b
John <johnconley507@hotmail.com>
2016-09-04 14:29:02 -0400

    logical expressions

--- sandbox/logical.php
+++ sandbox/logical.php
@@ -11,10 +11,17 @@
 
 			if ($a > $b) {
 				echo "a is larger than b";
+			} 
+		?>
+		<br />
+
+		<?php //Only welcome new users
+			$new_user = true;
+			if ($new_user) {
+				echo "<h1>Welcome!</h1>";
+				echo "<p>We are glad you decided to join us.</p>";
 			}
 		?>
-
-	
-
+		<br />
 	</body>
 </html>
