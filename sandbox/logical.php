039d982d7efb88451152948f50d531b7a8a4a83b
John <johnconley507@hotmail.com>
2016-09-04 14:51:24 -0400

    else elseif statements

--- sandbox/logical.php
+++ sandbox/logical.php
@@ -12,7 +12,7 @@
 			if ($a > $b) {
 				echo "a is larger than b";
 			} elseif ($a < $b) {
-				echo "a is snaller than b";
+				echo "a is smaller than b";
 			} else {
 				echo "a is equal to b";
 			}
