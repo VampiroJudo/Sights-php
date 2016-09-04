a633f2b0972466fffb7da52321f08c84a19a3d1e
John <johnconley507@hotmail.com>
2016-09-04 14:16:46 -0400

    adding logical.php page

---
+++ sandbox/logical.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Logical</title>
+	</head>
+	<body>
+
+		<?php 
+			$a = 4;
+			$b = 3;
+
+			if ($a > $b) {
+				echo "a is larger than b";
+			}
+		?>
+
+	
+
+	</body>
+</html>
