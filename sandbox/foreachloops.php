ecca996aac268e89d7bcd505c494897b6634bfcd
John <johnconley507@hotmail.com>
2016-09-06 16:52:39 -0400

    more foreach loop practice

--- sandbox/foreachloops.php
+++ sandbox/foreachloops.php
@@ -10,8 +10,41 @@
 			foreach($ages as $age) {
 				echo "Age: {$age}<br />";
 			}
+		?>
+		<br />
+		<?php // foreach using an associative array
 
-		?>	
+			$person = array(
+						"first_name" => "John",
+						"last_name" => 'Conley',
+						"address" => '123 Hacker Way',
+						"city" => 'Miami',
+						"state" => "Fl",
+						"zip_code" => '33333'
+					);
+			foreach($person as $attribute => $data) {
+				$attr_nice = ucwords(str_replace("_", " ", $attribute));
+				echo "{$attr_nice}: {$data}<br />";
+			}
+		?>
+		<br />
+
+		<?php
+			$prices = array("Brand New Computer" => 2000, 
+							"1 month of Lynda.com" => 25,
+							"Learning PHP" => null);
+			foreach($prices as $item => $price) {
+				echo "{$item}: ";
+				if (is_int($price)) {
+					echo "$" . $price;
+				} else {
+					echo "priceless";
+				}
+				echo "<br />"; 
+			  }
+		?>
+
+
 
 	</body>
 </html>
