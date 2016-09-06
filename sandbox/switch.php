ca6609d1fabf509c8cac4e0fad9db6be8fb92929
John <johnconley507@hotmail.com>
2016-09-06 15:05:49 -0400

    uploading switch statements page

---
+++ sandbox/switch.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SWITCH</title>
+	</head>
+	<body>
+		<?php
+			$a = 3;
+
+			switch ($a) {
+				case 0:
+					echo "a eqauls 0<br />";
+				case 1:
+					echo "a equals 1<br />";
+				case 2:
+					echo "a equals 2<br />";
+				case 3: 
+					echo "a equals 3<br />";
+			}
+		?>
+	
+
+	</body>
+</html>
