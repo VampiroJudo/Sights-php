e570f7a84d6752e8a327bb68330441a4e1b006dd
John <johnconley507@hotmail.com>
2016-09-08 16:01:48 -0400

    working with local and global variables

---
+++ sandbox/functions_scope.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Basic HTML</title>
+	</head>
+	<body>
+
+		<?php
+
+			$bar = "outside"; //global scope
+
+			function foo(){
+				global $bar;
+				if (isset($bar)) {
+					echo "foo: " . $bar . "<br />";
+				}
+				$bar = "inside"; //local scope
+			}
+
+			echo "1: " . $bar . "<br />";
+			foo();
+
+			echo "2: " . $bar . "<br />";
+		?>
+	
+
+	</body>
+</html>
