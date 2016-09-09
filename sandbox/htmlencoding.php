3e6f56338993515688f46721d8542babbf209019
John <johnconley507@hotmail.com>
2016-09-09 14:25:43 -0400

    url encoding pactice

---
+++ sandbox/htmlencoding.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HTML Encoding</title>
+	</head>
+	<body>
+
+		<?php 
+			$linktext = "<Click> & learn more";
+		?>
+
+		<a href="">
+			<?php echo htmlspecialchars($linktext); ?>
+		</a>
+		<br />
+
+		<?php 
+			
+			$text = "~&#%^";
+			echo htmlentities($text);
+
+		?>
+		<br />
+
+		<?php //what to use when
+
+			$url_page = "php/created/page/url.php";
+			$param1 = "This is a string with < >";
+			$param2 = "$%^&#%^ are bad characters";
+			$linktext = "<Click> & learn more";
+
+			$url = "http://localhost/";
+			$url .= rawurldecode($url_page);
+			$url .= "?" . "param1=" . urlencode($param1);
+			$url .= "&" . "param2=" . urlencode($param2);
+		?>
+
+		<a href="<?php echo htmlspecialchars($url); ?>">
+			<?php echo htmlspecialchars($linktext); ?>
+		</a>
+
+	</body>
+</html>
