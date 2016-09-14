077943cc7d5dff373694ebcc83145ba15e8bbadd
John <johnconley507@hotmail.com>
2016-09-14 15:34:35 -0400

    page redirection

---
+++ sandbox/redirect.php
@@ -0,0 +1,17 @@
+<?php 
+	// this is how you redirect to a new page
+	header("Location: basic.html");
+	exit;
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirect</title>
+	</head>
+	<body>
+	
+
+	</body>
+</html>
