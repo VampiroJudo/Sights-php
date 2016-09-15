6667463081d84c10aa8b338c38246937624cf746
John <johnconley507@hotmail.com>
2016-09-15 17:03:29 -0400

    bug fix for form.php file

--- sandbox/form.php
+++ sandbox/form.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Basic HTML</title>
+		<title>Form</title>
 	</head>
 	<body>
+
+		<form action="form_processing.php" method="post">
+			Username: <input type="text" name="username" value=""><br />
+			Password: <input type="password" name="password" value=""/><br />
+			<br />
+			<input type="submit" name="submit" value="Submit"/>
+		</form>
+
 	
 
 	</body>
