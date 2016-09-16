596634c5fcebbb4ef7f1b035808530362a8f9525
John <johnconley507@hotmail.com>
2016-09-16 14:08:34 -0400

    validations

---
+++ sandbox/validations.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Validations</title>
+	</head>
+	<body>
+
+	<?php
+
+	// * presence
+    // * string length
+    // * type
+	// * inclusion in set
+	// * uniqueness
+	// * format
+
+
+
+
+
+
+
+
+
+
+	?
+
+	
+
+	</body>
+</html>
