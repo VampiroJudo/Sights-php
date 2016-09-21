f5a4e564d38cb304a337641b65e21e39ec2fd92f
John <johnconley507@hotmail.com>
2016-09-21 15:48:39 -0400

    setting cookie values

--- sandbox/cookies.php
+++ sandbox/cookies.php
@@ -1,4 +1,5 @@
 <?php
+// REMEMBER: Setting cookies must be before *any* HTML output, unless output buffering is turned on.
 	$name = "test";
 	$value = 45;
 	$expire = time() + (60*60*24*7); // add seconds
@@ -11,7 +12,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title></title>
+		<title>Cookies</title>
 	</head>
 	<body>
 
