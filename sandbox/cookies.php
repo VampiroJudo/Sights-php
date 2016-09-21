22b37669455feef4fa961b0d4403ab4dfd4bee9b
John <johnconley507@hotmail.com>
2016-09-21 15:41:07 -0400

    daily updates

---
+++ sandbox/cookies.php
@@ -0,0 +1,22 @@
+<?php
+	$name = "test";
+	$value = 45;
+	$expire = time() + (60*60*24*7); // add seconds
+	setcookie($name, $value, $expire);
+?> 
+
+
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+
+
+	
+
+	</body>
+</html>
